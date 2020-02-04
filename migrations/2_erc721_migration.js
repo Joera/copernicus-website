0c1fea38b7f742b617977bb47d5fee142ab17771
Joera <joeramulders@gmail.com>
2020-02-04 20:49:59 +0100

    init

--- migrations/2_erc721_migration.js
+++
@@ -1,5 +0,0 @@
-const MyERC721 = artifacts.require("./MyERC721.sol");
-module.exports = async function(deployer) {
-    await deployer.deploy(MyERC721, "MyERC721 Token", "MyERC721")
-    const erc721 = await MyERC721.deployed()
-};
